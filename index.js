e89abc10265402411fdc9a3b63872957e2182a1e
Hiteshrudakiya <rudakiyahitesh96@gmail.com>
2025-07-14 10:57:47 +0530

    first two program complete

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log("hello world")
+
+//program 1 odd and even number
+// const no=7;
+// if(no%2==0){
+//     console.log("even number")
+// }
+// else{
+//     console.log("odd number")
+// }
+
+//program 2: find maximum of two number
+// const a=10
+// const b=20
+// if(a>b){
+//     console.log("a is big "+a)
+// }
+// else{
+//     console.log("b is big "+b)
+// }
